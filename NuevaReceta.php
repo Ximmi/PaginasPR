dde39b52d9f3575ddffc4109cea7a2094a986d07
Ximena Cervantes <ximmisweetximmi@gmail.com>
2019-11-27 12:52:14 -0600

    Add files via upload

---
+++ NuevaReceta.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+
+<?php
+        // put your code here
+?>
+<html>
+    <head>
+    <!-- Compiled and minified CSS -->
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa|Montserrat|Quicksand|Raleway&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <nav class="#eceff1 blue-grey lighten-5">
+            <div class="nav-wrapper">
+                <a href="../html/Index.php" class="brand-logo" style="font-family: 'Comfortaa'; color: #263238">EsComida</a>
+              <ul id="nav-mobile" class="right hide-on-med-and-down">
+                  <li><a href="" style="color: #263238">Perfil</a></li>
+                <li><a href=""style="color: #263238">Recetario</a></li>
+                <li><a href="" style="color: #263238">Cerrar sesión</a></li>
+              </ul>
+            </div>
+        </nav>
+  
+        <div class="row">
+            <div class="col s10 m6">
+              <div class="card">
+                <div class="card-content">
+                  <span class="card-title on-med">Paso 2</span>
+                  <h6 style="font-family: 'Montserrat'">Añade los ingredientes</h6>
+                  <p>Nombre, foto, origen</p>
+                </div>
+              </div>
+            </div>
+            <div class="col s10 m1">
+            </div>
+            <div class="col s10 m5">
+              <div class="card">
+                <div class="card-content">
+                    <span class="card-title">Paso 3</span>
+                    <h6 style="font-family: 'Montserrat'">Añade los pasos</h6>
+                    <p>Nombre, foto, origen</p>
+                </div>
+              </div>
+            </div>
+        </div>
+       
+       
+    </body>
+</html>
---
+++ NuevaReceta1.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<?php
+        // put your code here
+?>
+<html>
+    <head>
+    <!-- Compiled and minified CSS -->
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa|Montserrat|Quicksand|Raleway&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <nav class="#eceff1 blue-grey lighten-5">
+            <div class="nav-wrapper">
+                <a href="../html/Index.php" class="brand-logo" style="font-family: 'Comfortaa'; color: #263238">EsComida</a>
+              <ul id="nav-mobile" class="right hide-on-med-and-down">
+                  <li><a href="" style="color: #263238">Perfil</a></li>
+                <li><a href=""style="color: #263238">Recetario</a></li>
+                <li><a href="" style="color: #263238">Cerrar sesión</a></li>
+              </ul>
+            </div>
+        </nav>
+  
+        <div class="row">
+            <div class="col s10 m2">
+            </div>
+            
+            <div class="col s10 m8">
+              <div class="card">
+                <div class="card-content">
+                    <h3 style="font-family: 'Quicksand'">Agrega una receta</h3>
+                    <p style="font-family: 'Montserrat'">Completa los tres pasos para guardar tu receta</p>
+                    <span class="card-title">Paso 1</span>
+                    <h6 style="font-family: 'Montserrat'">Foto, nombre, origen</h6>
+                    <p>Nombre, foto, origen</p>
+                </div>
+              </div>
+            </div>
+            
+            <div class="col s10 m2">
+            </div>
+        </div>
+       
+    </body>
+</html>
